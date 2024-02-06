03b366cc9e18e635a95e468dee93b59ce9e3e205
Alex Zacharkiw <alexander.zacharkiw@dawsoncollege.qc.ca>
2024-02-06 14:10:22 -0500

    add base db

---
+++ server/db/db.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const dbUrl = process.env.ATLAS_URI;
+const { MongoClient } =  require("mongodb");
+
+let instance = null;
+
+class DB {
+  constructor() {
+    if (!instance) {
+      this.client = new MongoClient(dbUrl);
+      this.db = null;
+      
+      // add collections
+
+      instance = this;
+    }
+    return instance;
+  }
+
+  async connect(dbname, /* collections */ ){
+    if (instance.db) {
+      return;
+    }
+
+    await instance.client.connect();
+    instance.db = await instance.client.db(dbname);
+
+    // connect to collections
+  }
+
+  async close() {
+    await instance.client.close();
+    instance = null;
+  }
+
+}
