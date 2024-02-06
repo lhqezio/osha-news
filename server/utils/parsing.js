1d052f2984a9a47b09b2286a623d0b06b2e44a66
onipicotte <onipicotte@gmail.com>
2024-02-06 15:32:47 -0500

    added parsing script

---
+++ server/utils/parsing.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+
+const parseData = async () => {
+  try {
+    const dataPath = 'data/News_Category_Dataset_v3.json';
+    const newDataPath = 'data/parsedData.json';
+    const data = await fs.readFile(dataPath);
+    const stringData = data.toString();
+
+    const lines = stringData.split(/(\n|\r\n)/);
+
+    const newContent = `[${lines.filter((line) => line.length > 1).join(',\n')}]`;
+
+    fs.writeFile(newDataPath, newContent);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+};
+
+(async () => {
+  await parseData();
+})();
