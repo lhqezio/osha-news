a9c7331e41312bc083e31b8a7a0527a57826d9ea
Alex Zacharkiw <alexander.zacharkiw@dawsoncollege.qc.ca>
2024-02-07 10:32:03 -0500

    add working seeding

--- server/utils/seeding.js
+++ server/utils/seeding.js
@@ -8,14 +8,17 @@ const db = new DB();
 const seeding = async() => {
   try{
     await db.connect('news', 'newsArticles');
-    
-    const fileData = fs.readFile(path.join(__dirname, '../data/parsedData.json'));
+
+    const fileData = await fs.readFile(path.join(__dirname, '../data/parsedData.json'));
 
     const jsonData = await JSON.parse(fileData);
 
+    await db.createManyNewsArticles(jsonData);
   } catch (err) {
     console.error(err);
   } finally {
     db.close();
   }
 };
+
+seeding();
