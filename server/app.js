76991aaa4c5a1a3371ac852030e60f63931afdf4
Hoang Quoc Le <hoang.le@dawsoncollege.qc.ca>
2024-02-07 11:04:57 -0500

    Modify server to serve front end build

--- server/app.js
+++ server/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const path= require('path')
+const helloRoute = require('./routes/helloworldRoute');
+
 
 const app = express();
 
+const _filename = 
+__filename || typeof require !== 'undefined' && require('url').fileURLToPath || '';
+const _dirname = __dirname || path.dirname(_filename)
+
+// Add middleware to serve static files
+app.use(express.static(path.join(path.dirname(_filename), '..', 
+  'client', 'build')));
+
 // Hello world route
-const helloRoute = require('./routes/helloworldRoute');
 app.use('/hello', helloRoute);
 
-app.get('/', (req, res) => {
-  res.send('Server ON');
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(path.dirname(_filename), '..', 
+    'client', 'build', 'index.html'));
 });
 
 app.use((req, res) => {
