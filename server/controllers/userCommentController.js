01e9b2911c2db063c54dd52740df3557ddfbba4e
onipicotte <onipicotte@gmail.com>
2024-02-10 16:39:18 -0500

    implemented post request

--- server/controllers/userCommentController.js
+++ server/controllers/userCommentController.js
@@ -1,7 +1,21 @@
+const DB = require('../db/db');
+
+const db = new DB();
+
 module.exports.getUsers = async (req, res) => {
   res.send('User');
 };
 
 module.exports.addUser = async (req, res) => {
-  res.send(req.body);
+  try {
+    await db.connectToCollection('news', 'userComment');
+  
+    const userComment = req.body;
+  
+    await db.createUserComment(userComment);
+  
+    res.status(201).json({'status': 'User Comment as successfully been added.'});
+  } catch (err) {
+    res.status(500).json({'error': 'Could not add user comment.'});
+  }
 };
