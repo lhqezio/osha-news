5cabd2e4ce24f65c0d48efc69e32bf380b2ab9ea
SettMann <s.mannarino137@gmail.com>
2024-02-12 03:51:38 -0500

    added the form for uploading images

---
+++ client/src/Image/ImagePage.js
@@ -0,0 +1,94 @@
+import {useState,useEffect,useRef} from 'react'
+import useInput from '../CustomHooks/useInput'
+
+function ImagePage() {
+  const [imagePostError, setImagePostError] = useState('')
+  const [imageFetchError, setImageFetchError] = useState('')
+  const [username, userInput] = useInput({ type: "text" });
+  const inputImage = useRef(0)
+  const [images, setImages] = useState([])
+
+    useEffect(
+        ()=>{
+            fetch('/user-image')
+                .then((resp)=>{
+                    if(!resp.ok) {
+                        setImageFetchError("Connection Error Occurred")
+                    }
+                    else {
+                        return resp.json()
+                    }
+                })
+                .then ((json)=>{
+                    setImages(json)
+                })
+                .catch ((err)=>{
+                    setImageFetchError("Server Error Occured")
+                })
+        },[]
+    )
+
+    function convertToBlob() {
+      fetch(inputImage.current.value)
+      .then((response) => {
+        return response.blob()
+      })
+      .then((blob) => {
+        // send blob to azure and then save uri
+      });
+    }
+
+    function postImage() {
+        if(username && inputImage.current.value !== "") {
+            fetch('/user-image', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: username,
+                    image: inputImage.current.value,
+                })
+            }).then(
+                (resp)=>{
+                    setImagePostError("Connection Error Occured")
+                }
+            ).catch(
+                (err)=>{
+                    setImagePostError("Server Error Occured")
+                }
+            )
+        } else {
+            setImagePostError("One or more required fields are empty")
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <h2>
+                    All Images:
+                </h2>
+                <div>
+                UserName<br />
+                {userInput}
+                </div>
+                <div>
+                <label for="avatar">Image</label><br />
+                <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" ref={inputImage}/>  
+                </div>
+                <button onClick={postImage} type="button">Post Comment</button>
+                {imagePostError ? <div>{imagePostError}</div> : null}
+            </div>
+            <div>
+                <h2>
+                    All Images
+                </h2>
+                {imageFetchError ? <div>{imageFetchError}</div>:null}
+            </div>
+        </>
+    )
+}
+
+export default ImagePage
