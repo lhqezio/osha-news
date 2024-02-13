666257603194de6b6895fa0cf4c309db933ed607
SettMann <s.mannarino137@gmail.com>
2024-02-13 15:21:26 -0500

    the http post now happens in the postImage function

--- client/src/Image/ImagePage.js
+++ client/src/Image/ImagePage.js
@@ -7,6 +7,7 @@ function ImagePage() {
   const [username, userInput] = useInput({ type: "text" });
   const inputImage = useRef(0);
   const inputUser = useRef(0);
+  const form = useRef(0);
   const [images, setImages] = useState([]);
 
     useEffect(
@@ -29,35 +30,57 @@ function ImagePage() {
         },[]
     );
 
-    // async function postImage() {
-    //   console.log(inputUser.current.value);
-    //   let jsonFile = inputImage.current.files[0];
-    //   const blob = await new Blob([jsonFile], { type: "application/json" });
-    //   if(inputUser.current.value !== "" && blob) {
-    //       fetch('/user-images', {
-    //           method: 'POST',
-    //           headers: {
-    //               'Accept': 'application/json',
-    //               'Content-Type': 'application/json',
-    //           },
-    //           body: JSON.stringify({
-    //               username: inputUser.current.value,
-    //               image: blob,
-    //           })
-    //       }).then(
-    //           (resp)=>{
-    //               setImagePostError("Connection Error Occured");
-    //           }
-    //       ).catch(
-    //           (err)=>{
-    //               setImagePostError("Server Error Occured");
-    //           }
-    //       );
-    //   } else {
-    //       setImagePostError("One or more required fields are empty");
-    //   }
-    // }
+    async function postImage() {
+        console.log("here")
+        let jsonFile = inputImage.current.files[0];
+        const blob = await new Blob([jsonFile], { type: "application/json" });
+        const formdata = new FormData(form.current);
+        if(formdata) {
+            fetch('/user-images', {
+                method: 'POST',
+                body: formdata
+            }).then(
+                (resp)=>{
+                    setImagePostError("Connection Error Occured");
+                }
+            ).catch(
+                (err)=>{
+                    setImagePostError("Server Error Occured");
+                }
+            );
+        } else {
+            setImagePostError("One or more required fields are empty");
+        }
+    }
 
+    // return (
+    //     <>
+    //         <div>
+    //             <h2>
+    //                 All Images:
+    //             </h2>
+    //             <div>
+    //             </div>
+    //             <div>
+    //                 <form method="POST" action="http://localhost:3001/user-images" enctype="multipart/form-data">
+    //                     <input type="text" name="user" ref={inputUser}/>
+    //                     <input type="file" id="avatar" name="file" ref={inputImage}/>  
+    //                     <input type="submit"/>
+    //                 </form>
+    //             </div>
+    //         </div>
+    //         <div>
+    //             <h2>All Images</h2>
+    //             <ul>
+    //             {images.map((img) => (
+                    
+    //               <li key={img.user}><p>{img.username}</p><img src= {img.url} alt="userImage"></img></li>
+    //             ))}
+    //             </ul>
+    //             {imageFetchError ? <div>{imageFetchError}</div>:null}
+    //         </div>
+    //     </>
+    // )
     return (
         <>
             <div>
@@ -67,10 +90,11 @@ function ImagePage() {
                 <div>
                 </div>
                 <div>
-                    <form method="POST" action="http://localhost:3001/user-images" enctype="multipart/form-data">
+                    <form ref={form}>
                         <input type="text" name="user" ref={inputUser}/>
                         <input type="file" id="avatar" name="file" ref={inputImage}/>  
                         <input type="submit"/>
+                        <button type="button" onClick={postImage}>Submit</button>
                     </form>
                 </div>
             </div>
