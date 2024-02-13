1b0ecef838c05ae912613a6138066d7c4f45bef5
onipicotte <onipicotte@gmail.com>
2024-02-13 14:06:28 -0500

    cleared useless code

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -2,9 +2,6 @@ const request = require('supertest');
 const { describe, expect, test } = require('@jest/globals');
 
 const app = require('../app');
-const DB = require('../db/db');
-
-jest.mock('../db/db');
 
 describe('Hello World Route', () => {
   test('Should return Hello World!', async () => {
