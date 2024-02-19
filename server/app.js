ad8fd3b0ce4721b3118d575758e6a0343c935ee7
onipicotte <onipicotte@gmail.com>
2024-02-19 10:43:44 -0500

    resolved conflict

--- server/app.js
+++ server/app.js
@@ -5,11 +5,4 @@ const app = express();
 app.use(express.static('../client/build'));
 app.use(express.json());
 
-<<<<<<< HEAD
-// random article endpoint
-
-// category options endpoint
-
-=======
 module.exports = app;
->>>>>>> 601704881fc1da315a80fde21fbf3c2b1b8f3553
