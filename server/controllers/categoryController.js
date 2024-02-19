b87ecfd2182c4501fb35749cf06d01d013a9810b
Alex Zacharkiw <alexander.zacharkiw@dawsoncollege.qc.ca>
2024-02-19 10:44:15 -0500

    add export

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -2,7 +2,7 @@ const DB = require('../db/db');
 
 const db = DB();
 
-module.exports.getAllCategories = async (req, res) => {
+module.exports.getAllCategories = async (res) => {
   const categories = await db.getAllCategories();
   res.send(categories);
 };
