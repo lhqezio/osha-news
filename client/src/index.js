8cb755aa7b6f3b102fc6d9b3fa9de9d367c557a6
Hoang Quoc Le <hoang.le@dawsoncollege.qc.ca>
2024-02-21 10:06:49 -0500

    Fix stuffs

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root';
+import './index.css';
 
 const router = createBrowserRouter([
   {
