26d54e7676c288919b63377079f5430c16e815b1
Alex Zacharkiw <alexander.zacharkiw@dawsoncollege.qc.ca>
2024-02-21 09:40:04 -0500

    add error handling to search

--- server/controllers/searchArticlesControllers.js
+++ server/controllers/searchArticlesControllers.js
@@ -1,5 +1,10 @@
 module.exports.searchAllArticles = async (req, res) => {
-  const searchType = req.params.searchType;
-  const searchValue = req.params.searchValue;
-  res.status(200).json({'search method' : searchType, 'result' : searchValue });
+  try{
+    const searchType = req.params.searchType;
+    const searchValue = req.params.searchValue;
+    res.status(200).json({'search method' : searchType, 'result' : searchValue });
+  } catch (err) {
+    res.status(500).json({ 'error' : 'Internal Error'});
+  }
+
 };
