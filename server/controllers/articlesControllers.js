a3cd8aa85f9624e9163b906d58be445654013209
Alex Zacharkiw <alexander.zacharkiw@dawsoncollege.qc.ca>
2024-02-26 10:46:49 -0500

    add new model for articles

--- server/controllers/articlesControllers.js
+++ server/controllers/articlesControllers.js
@@ -1,10 +1,8 @@
-const DB = require('../db/db');
-
-const db = new DB();
+const {getOneArticle} = require('../db/db');
 
 module.exports.getOneArticle = async (req, res) => {
   try {
-    const article = await db.getOneArticle();
+    const article = await getOneArticle();
 
     res.status(200).json(article);
   } catch (err) {
--- server/db/db.js
+++ server/db/db.js
@@ -1,141 +1,101 @@
 require('dotenv').config();
 const dbUrl = process.env.ATLAS_URI;
-const { MongoClient } =  require('mongodb');
+const mongoose = require('mongoose');
 
-let instance = null;
-
-class DB {
-  constructor() {
-    if (!instance) {
-      this.client = new MongoClient(dbUrl);
-      this.db = null;
-      
-      // add collections
-      this.collection = null;
-
-      instance = this;
-    }
-    return instance;
-  }
-
-  /**
-   * Connect to specified database
-   * @param dbname name of the database
-   */
-  async connect(dbname){
-    if (instance.db) {
-      return;
-    }
-
-    await instance.client.connect();
-    instance.db = await instance.client.db(dbname);
-
-    // Connect to collections
-    this.createAllCollection();
-    instance.newsArticles = await instance.db.collection('newsArticles');
-    instance.userComments = await instance.db.collection('userComments');
-    instance.userImages = await instance.db.collection('userImages');
-  }
-
-  /**
-   * Create all collection needed for the app.
-   */
-  async createAllCollection() {
-    if (!instance.db) {
-      return;
-    }
-
-    const collNames = await instance.db.listCollections().toArray();
-
-    if (collNames.filter(coll => coll.name === 'newsArticles').length === 0) {
-      await instance.db.createCollection('newsArticles');
-    }
-    if (collNames.filter(coll => coll.name === 'userComments').length === 0) {
-      await instance.db.createCollection('userComments');
-    }
-    if (collNames.filter(coll => coll.name === 'userImages').length === 0) {
-      await instance.db.createCollection('userImages');
-    }
-  }
-
-  /**
-   * Close database connection
-   */
-  async close() {
-    await instance.client.close();
-    instance = null;
-  }
-
-  /**
-   * Add many rows of news data
-   * @param articles list article to add to newsArticles
-   */
-  async createManyNewsArticles(articles) {
-    await instance.newsArticles.insertMany(articles);
-  }
-
-  /**
-   * Get one article
-   */
-  async getOneArticle() {
-    const article = await instance.newsArticles.findOne();
-    return article;
-  }
-
-  /**
-   * Remove from the Database using filter.
-   * @param filter filter for the delete
-   * @returns amount of element deleted
-   */
-  async deleteMany(filter) {
-    const newsArticlesResult = await instance.newsArticles.deleteMany(filter);
-    const userCommentsResult = await instance.userComments.deleteMany(filter);
-    const userImagesResult = await instance.userImages.deleteMany(filter);
-    return  newsArticlesResult.deletedCount + 
-            userCommentsResult.deletedCount + 
-            userImagesResult.deletedCount;
-  }
-
-  /**
-   * Add one user-comment
-   * @param comment single comment to add to userComments
-   */
-  async createUserComment(comment) {
-    await instance.userComments.insertMany([comment]);
-  }
-
-  /**
-   * Get userComments
-   * @param filter filter of the search
-   * @returns comments found 
-   */
-  async getUserComments(filter) {
-    const comments = await instance.userComments.find(filter).toArray();
-    return comments;
-  }
-
-  /**
-   * Add one user-image
-   * @param image single image to add to userImage
-   */
-  async createUserImage(image){
-    await instance.userImages.insertMany([image]);
-  }
-
-  /**
-   * Get userImages
-   * @param filter filter of the search
-   * @returns images found
-   */
-  async getUserImages(filter){
-    const images = await instance.userImages.find(filter).toArray();
-    return images;
-  }
-
-  async getCategories() {
-    const categories = await instance.newsArticles.distinct('category');
-    return categories;
-  }
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(dbUrl);
 }
 
-module.exports = DB;
+const articleSchema = mongoose.Schema({
+  link: String,
+  headline: String,
+  category: String,
+  short_description: String,
+  authors: String,
+  date: String,
+  image: String
+});
+
+const ArticleModel = new mongoose.model('newsArticles', articleSchema);
+
+const newArticle = new ArticleModel({
+  link: 'test',
+  headline: 'test',
+  category: 'test',
+  short_description: 'test',
+  authors: 'test',
+  date: 'today',
+  image: 'test'
+});
+
+(async function test(){
+  await newArticle.save();
+})();
+
+
+/**
+  * Add many rows of news data
+  * @param articles list article to add to newsArticles
+  */
+module.exports.createManyNewsArticles = async(articles) => {
+  await ArticleModel.insertMany(articles);
+};
+
+/**
+ * Remove from the Database using filter.
+ * @param filter filter for the delete
+ * @returns amount of element deleted
+ */
+module.exports.emptyDatabase = async() => {
+  await ArticleModel.deleteMany();
+};
+
+/**
+ * Get one article
+ */
+module.exports.getOneArticle = async() => {
+  const article = await ArticleModel.findOne();
+  return article;
+};
+
+// /**
+//  * Add one user-comment
+//  * @param comment single comment to add to userComments
+//  */
+// async createUserComment(comment) {
+//   await instance.userComments.insertMany([comment]);
+// }
+
+// /**
+//  * Get userComments
+//  * @param filter filter of the search
+//  * @returns comments found 
+//  */
+// async getUserComments(filter) {
+//   const comments = await instance.userComments.find(filter).toArray();
+//   return comments;
+// }
+
+// /**
+//  * Add one user-image
+//  * @param image single image to add to userImage
+//  */
+// async createUserImage(image){
+//   await instance.userImages.insertMany([image]);
+// }
+
+// /**
+//  * Get userImages
+//  * @param filter filter of the search
+//  * @returns images found
+//  */
+// async getUserImages(filter){
+//   const images = await instance.userImages.find(filter).toArray();
+//   return images;
+// }
+
+module.exports.getCategories = async() => {
+  const categories = await ArticleModel.distinct('category');
+  return categories;
+};
