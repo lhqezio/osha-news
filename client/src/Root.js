c64f0e229bb58a19863ae0c236789741774de02a
SettMann <s.mannarino137@gmail.com>
2024-02-26 02:40:55 -0500

    adds nav styling all thats left is spacing

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,26 +1,29 @@
 import { Outlet, Link } from 'react-router-dom';
 import ShortScroll from './Article/ShortScroll';
+import home from './images/home.png';
+import search from './images/search.png';
+import avatar from './images/avatar.png';
 
 export default function Root(){
   return (
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link to={`/`}><img src='../images/home.png' alt='home icon'></img></Link>
+        <ul class="flex flex-row justify-between">
+          <li class="inline w-1/3">
+            <div class="flex flex-row">
+              <Link to={`/`}><img class="w-8 h-7 p-1" src={home} alt="home icon"/></Link>
+              <img class="w-8 h-7 p-1" src={search} alt="search icon"/>
+              <input class="border" type="text" value="Search"/>
+            </div>
           </li>
-          <li>
-            <img src='../images/search.png' alt='search icon'></img>
-          </li>
-          <li>
-            <input type='text' value='Search'/>
-          </li>
-          <li>
+          <li class="inline w-1/3">
             <h1>OSHA News</h1>
           </li>
-          <li>User</li>
-          <li>
-            <img src='../images/avatar.png'></img>
+          <li class="inline w-1/3">
+            <div class="flex flex-row items-end">
+              <h1>User</h1>
+              <img class="w-9 h-7" src={avatar} alt="profile picture"/>
+            </div>
           </li>
         </ul>
       </nav>
