cde7300c8c06773f008d5ed944e75e459f4d850a
Hoang Quoc Le <hoang.le@dawsoncollege.qc.ca>
2024-02-27 13:39:50 -0500

    Nice article display

--- client/src/Article/ArticleDetail.js
+++ client/src/Article/ArticleDetail.js
@@ -1,7 +1,26 @@
-export default function ArticleDetail() {
+export default function Article({article}) {
   return (
-    <div className="section bg-green-700 w-10/12 h-10/12">
-        1
-    </div>
+
+    <section 
+      style={{ backgroundImage : `url('${article.image}')`}}
+      class={'snap-start h-[80vh] rounded-xl p-4 bg-no-repeat bg-cover bg-center bg-fixed'} >
+      <div className="w-1/2 backdrop-blur-lg p-6 drop-shadow-md rounded-lg">
+        <div className="text-4xl font-serif">
+          {article.headline}
+        </div>
+        <div>
+          {article.short_description}
+        </div>
+        <div>
+          {article.authors}
+        </div>
+        <div>
+          {article.date}
+        </div>
+        <div>
+          <a className="text-blue-600" href={`${article.link}`}>Read More</a>
+        </div>
+      </div>
+    </section>
   );
 }
--- client/src/Article/ShortScroll.js
+++ client/src/Article/ShortScroll.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Article from './ArticleDetail';
 
 export default function ShortScroll(props) {
   const [fetchErrMsg, setFetchErrMsg] = useState();
@@ -35,18 +36,12 @@ export default function ShortScroll(props) {
   return (
     <div>
       {fetchErrMsg ? <div>{fetchErrMsg}</div> : null}
-      {true ? 
-        <div className="w-3/4 h-[80vh] snap-mandatory snap-y overflow-auto 
+      {article !== null ? 
+        <div className="w-11/12 h-[80vh] snap-mandatory snap-y overflow-auto 
         my-0 mx-auto no-scrollbar::-webkit-scrollbar no-scrollbar scroll-smooth 
-        rounded-xl">
-          <section class="snap-start h-[80vh] bg-red-900">
-            Page one
-          </section>
-          <section class="snap-start h-[90vh] bg-green-600">
-            Page two
-          </section>
+        rounded-xl shadow-md">
+          <Article article={article}></Article>
         </div> : null}
-      <p>ShortScroll</p>
     </div>
   );
 }
