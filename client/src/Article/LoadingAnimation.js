9d707cc2c527194dd49da16db552af432e7d7c6f
Hoang Quoc Le <hoang.le@dawsoncollege.qc.ca>
2024-02-28 10:42:26 -0500

    Complete Infinite Scroll - Improve theming - Bug fixes

---
+++ client/src/Article/LoadingAnimation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactLoading from 'react-loading';
+ 
+const LoadingAnimation = ({ type, color }) => (
+  <ReactLoading type={type} color={color} height={'10vw'} width={'10vw'} 
+    className="my-0 mx-auto self-center"/>
+);
+ 
+export default LoadingAnimation;
--- client/src/Article/ShortScroll.js
+++ client/src/Article/ShortScroll.js
@@ -27,7 +27,7 @@ export default function ShortScroll(props) {
   return (
     <div className="w-11/12 h-[80vh] snap-mandatory snap-y overflow-auto 
         my-0 mx-auto no-scrollbar::-webkit-scrollbar no-scrollbar scroll-smooth 
-        rounded-xl shadow-md">
+        rounded-xl shadow-md border-t-2 border-slate-100">
       
       {
         articleElems.map(
