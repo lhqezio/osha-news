929181ef939debdb6c4025e6930e749ab0a59daa
SettMann <s.mannarino137@gmail.com>
2024-03-06 09:41:01 -0500

    added root to post artcle page

---
+++ client/src/Post/Post.jsx
@@ -0,0 +1,9 @@
+// import { Link } from 'react-router-dom';
+
+export default function PostArticle(){
+  return (
+    <div>
+      <p>Post Page</p>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root';
+import PostArticle from './Post/Post.jsx';
 import ErrorPage from './error-page';
 import './index.css';
 
@@ -11,6 +12,11 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: '/post',
+    element: <PostArticle />,
+    errorElement: <ErrorPage />,
+  }
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
